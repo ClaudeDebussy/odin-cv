647e6c69f8969fc28b2dc691427fee2b16fda212
ClaudeDebussy <15807600+ClaudeDebussy@users.noreply.github.com>
2025-12-18 09:52:25 +0500

    set up project

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import "./modern-css-reset.css";
-import App from "./App.jsx";
+import "./styles/index.css";
+import "./styles/modern-css-reset.css";
+import App from "./components/App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
