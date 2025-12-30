95a270d374a72058a472056f30338a5db16d49c0
ClaudeDebussy <15807600+ClaudeDebussy@users.noreply.github.com>
2025-12-30 14:23:51 +0500

    start fresh

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,51 +1,5 @@
 import { useState } from "react";
-import Form from "./Form";
 
 function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [school, setSchool] = useState("");
-  const [study, setStudy] = useState("");
-  const [dateOfStudy, setDateOfStudy] = useState("");
-
-  const handleName = (e) => setName(e.target.value);
-  const handleEmail = (e) => setEmail(e.target.value);
-  const handlePhone = (e) => setPhone(e.target.value);
-  const handleSchool = (e) => setSchool(e.target.value);
-  const handleStudy = (e) => setStudy(e.target.value);
-  const handleDateOfStudy = (e) => setDateOfStudy(e.target.value);
-
-  return (
-    <>
-      <Form
-        person={{
-          name,
-          email,
-          phone,
-          school,
-          study,
-          dateOfStudy,
-          handleName,
-          handleEmail,
-          handlePhone,
-          handleSchool,
-          handleStudy,
-          handleDateOfStudy,
-        }}
-      />
-      <div>
-        <h2>{name}</h2>
-        <h2>{email}</h2>
-        <h2>{phone}</h2>
-      </div>
-      <div>
-        <h2>{school}</h2>
-        <h2>{study}</h2>
-        <h2>{dateOfStudy}</h2>
-      </div>
-    </>
-  );
+  const [identity, setIdentity] = useState({ name: "", email: "", phone: "" });
 }
-
-export default App;
