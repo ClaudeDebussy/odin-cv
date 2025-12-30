a074b277b223a3118bee1361df299afaf18a6163
ClaudeDebussy <15807600+ClaudeDebussy@users.noreply.github.com>
2025-12-30 14:29:23 +0500

    begin work on handleIdentity

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,4 +2,11 @@ import { useState } from "react";
 
 function App() {
   const [identity, setIdentity] = useState({ name: "", email: "", phone: "" });
+
+  function handleIdentity(event) {
+    setIdentity({...identity, })
+  }
+
+  <IdentityForm />
+  <IdentityDisplay />
 }
